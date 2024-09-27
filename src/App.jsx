59f3768e081ac5328df05e73f29c63e9b87101ab
Viktor Morhun <morhun.fe@gmail.com>
2024-09-27 16:11:54 +0300

    add react app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import "./App.css";
-
+import Header from "./Header.jsx";
+import Banner from "./Banner.jsx";
+import Blog from "./Blog.jsx";
+import News from "./News.jsx";
 
 function App() {
   return (
     <>
+      <Header />
+      <Banner />
+      <div className="container">
+        <Blog />
+        <News />
+      </div>
     </>
   );
 }
